87b70d39f29d68bd65eb67f5b8ff30109671d7f6
Param Singh <paramsingh258@gmail.com>
2015-01-11 18:18:13 +0530

    Optimized gcdq.c

--- src/gcdq.c
+++ src/gcdq.c
@@ -2,11 +2,11 @@
 
 int hcf( int a, int b )
 {
-	while( a != b ){
-		if( a > b )
-			a = a - b;
-		else
-			b = b - a;
+	int tmp;
+	while(b){
+		tmp = a % b;
+		a = b;
+		b = tmp;
 	}
 	return a;
 }
@@ -29,8 +29,28 @@ int main( void )
 			int j;
 			scanf("%d %d", &l, &r);
 			int newsize = n - (r - l + 1);
+			if( newsize == 1){
+				if(l==1)
+					printf("%d\n", arr[r+1]);
+				else
+					printf("%d\n", arr[l-1]);
+				continue;
+			}
+			int tmp=0;
+			if(l==2)
+				tmp = hcf(arr[1],arr[r+1]);
+			else if(l==1)
+				tmp = hcf(arr[r+1], arr[r+2]);
+			else
+				tmp = hcf(arr[1],arr[2]);
+
+			for(j=1;j<=n;j++){
+				if(j<l || j>r)
+					tmp = hcf(tmp, arr[j]);
+			}
+
 			/* if only one element is left after slicing */
-			int sliced[newsize], count = 0;
+			/*int sliced[newsize], count = 0;
 			for(j=1;j<=n;j++){
 				if(j<l || j>r)
 					sliced[count++] = arr[j];
@@ -42,7 +62,8 @@ int main( void )
 			int tmp = hcf(sliced[0], sliced[1]);
 			for(j=2;j<newsize;j++){
 				tmp = hcf(tmp, sliced[j]);
-			}
+			}*/
+
 
 			printf("%d\n", tmp);
 
