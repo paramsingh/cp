3c808807cb7841a3097f0c06431359b5e1aa41e4
Param Singh <paramsingh258@gmail.com>
2015-04-07 03:07:24 +0530

    Optimize cseq.hs slightly more.

--- src/april2015/cseq.hs
+++ src/april2015/cseq.hs
@@ -2,15 +2,22 @@ import Control.Monad
 import Data.List
 import qualified Data.ByteString.Char8 as B
 
+-- Brute force method
 ans :: Int -> Integer -> Integer -> Integer
 ans n l r = (sum . concat . (take n) $ requiredlst l r) `mod` (10^6 + 3)
 
 requiredlst l r = [r - l + 1] : iterate generate [1 .. (r-l+1)]
                   where generate lst = 1 : zipWith (+) (tail lst) (generate lst)
-{-
-(\x -> 1 : zipWith (+) ta)
-reqList l r = [r-l+1] : iterate (\x -> (map sum) . init $ tails x) [(r-l+1), (r-l) .. 1]
---}
+
+
+-- Slightly more optimised
+facts = 1 : 1 : 2 : zipWith (*) (drop 2 facts) [3..]
+
+n `c` r = (facts !! n) `div` ((facts !! (n-r)) * (facts !! r))
+
+anotherList l r = map (`mod` (10^6+3)) $ zipWith (c) [(r-l+1) .. ] [1..]
+
+ans' n l r = (sum . (take n) $ anotherList l r) `mod` (10^6 + 3)
 
 main = readLn >>= flip replicateM_ secondIO
 
