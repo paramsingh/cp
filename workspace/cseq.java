3b1a9df4f181278675857bc8446d0b3cceaf0dfd
Param Singh <paramsingh258@gmail.com>
2015-04-08 12:05:32 +0530

    Maybe faster.

--- workspace/cseq.java
+++ workspace/cseq.java
@@ -17,27 +17,32 @@ class CSEQ
                 System.out.println(diff);
                 continue;
             }
-            long constdenfact = factmodbig(diff-1);
-            long prevnumfact = ((constdenfact) * (diff % big)) % big;
-            long prevdenfact = 1;
+            long constDenFact = factModBig(diff-1);
+            long prevNumFact = ((constDenFact) * (diff % big)) % big;
+            long prevDenFact = 1;
             long ans = 0;
+            long inverse = invertMod(prevDenFact);
             for(i=1;i<=n;i++){
-                ans += ((prevnumfact % big) * invert_mod(prevdenfact)) % big;
-                prevnumfact = ((prevnumfact % big) * ((diff+i)%big)) % big;
-                prevdenfact = ((prevdenfact % big) * ((i+1) % big)) % big;
+                ans += ((prevNumFact % big) * inverse) % big;
+                prevNumFact = ((prevNumFact % big) * ((diff+i) % big)) % big;
+                inverse = (inverse * (invertMod(i+1) % big)) % big;
             }
             ans = ans % big;
-            ans = ((ans % big) * invert_mod(constdenfact)) % big;
+            ans = ((ans % big) * invertMod(constDenFact)) % big;
             System.out.println(ans);
         }
     }
-    static long factmodbig(long n){
+
+
+    static long factModBig(long n){
         long fact = 1;
         for(long i=2; i<=n; i++)
             fact = ((fact % big) * (i%big)) % big;
         return (fact % big);
     }
-    static long invert_mod(long a) {
+
+
+    static long invertMod(long a) {
         long ex = big-2, result = 1;
         while (ex > 0) {
             if (ex % 2 == 1) {
