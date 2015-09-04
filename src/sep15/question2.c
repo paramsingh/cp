e2e59f60129be4845963714e08c239db77d8068d
Param Singh <paramsingh258@gmail.com>
2015-09-04 20:06:08 +0530

    Apparently entire logic is wrong. (10 pts)

--- src/sep15/question2.c
+++ src/sep15/question2.c
@@ -1,5 +1,4 @@
 // Author: Param Singh <paramsingh258@gmail.com>
-
 #include <stdio.h>
 
 int main(void) {
@@ -7,25 +6,23 @@ int main(void) {
 	scanf("%d", &t);
 	int a;
 	for (a = 0; a < t; a++) {
-		int n, m;
-		scanf("%d %d", &n, &m);
-		int ans = 0;
-		int i;
-		int flag = 0;
+		long long int n, m;
+		scanf("%lld %lld", &n, &m);
+		long long int ans = 0;
+		long long int i, j;
 		int ones = 0;
 		for (i = 0; i < m; i++) {
-			int j;
-			scanf("%d", &j);
+			scanf("%lld", &j);
 			if (j == 1)
 				ones ++;
 		}
-		int not_ones = m - ones;
-		while (m != 1) {
-			ans ++;
+		long long int not_ones = m - ones;
+		while (m > 1) {
+			ans++;
 			if (ones > 0) {
 				if (m > 1){
 					m -= 2;
-					ones --;
+					ones--;
 				}
 				else {
 					m--;
@@ -33,9 +30,9 @@ int main(void) {
 				}
 			}
 			else {
-				m -= 1;
+				m--;
 			}
 		}
-		printf("%d\n", ans);
+		printf("%lld\n", ans);
 	}
 }
