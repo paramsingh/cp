7dbe25a76a6288b16d152aef805b9af8c9a314f5
Param Singh <paramsingh258@gmail.com>
2015-09-05 00:10:43 +0530

    Changes to logic. 40 points now

--- src/sep15/question2.c
+++ src/sep15/question2.c
@@ -1,38 +1,40 @@
 // Author: Param Singh <paramsingh258@gmail.com>
 #include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    int *p = (int *) a;
+    int *q = (int *) b;
+    return (*p) - (*q);
+}
 
 int main(void) {
 	int t;
 	scanf("%d", &t);
 	int a;
 	for (a = 0; a < t; a++) {
-		long long int n, m;
-		scanf("%lld %lld", &n, &m);
-		long long int ans = 0;
-		long long int i, j;
-		int ones = 0;
-		for (i = 0; i < m; i++) {
-			scanf("%lld", &j);
-			if (j == 1)
-				ones ++;
-		}
-		long long int not_ones = m - ones;
-		while (m > 1) {
-			ans++;
-			if (ones > 0) {
-				if (m > 1){
-					m -= 2;
-					ones--;
-				}
-				else {
-					m--;
-					ones--;
-				}
-			}
-			else {
-				m--;
-			}
-		}
-		printf("%lld\n", ans);
+		int n, m;
+		scanf("%d %d", &n, &m);
+	    int arr[m] ;
+        int j;
+        for (j = 0; j < m; j++){
+            scanf("%d", &arr[j]);
+        }
+        qsort(arr, m, 4, compare);
+        int start = 0;
+        int end = m-1;
+        int ans = 0;
+        while (start < end) {
+            ans++;
+            if (arr[start] == 1) {
+                start++;
+                end--;
+            }
+            else {
+                arr[start]--;
+                end--;
+            }
+        }
+        printf("%d\n", ans);
 	}
 }
