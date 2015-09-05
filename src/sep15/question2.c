5da0d6c0d8e78446f9896caa1d2993b6584e24e1
Param Singh <paramsingh258@gmail.com>
2015-09-05 11:34:12 +0530

    Used getchar_unlocked and got 100 points (Fuck you codechef!)

--- src/sep15/question2.c
+++ src/sep15/question2.c
@@ -7,17 +7,31 @@ int compare(const void* a, const void* b) {
     return (*p) - (*q);
 }
 
+
+void fastread( int *a){
+    char c=0;
+    while(c<33){
+        c=getchar_unlocked();
+    }
+    *a=0;
+    while(c>33){
+        *a=*a*10+c-'0';
+        c=getchar_unlocked();
+    }
+}
+
 int main(void) {
 	int t;
-	scanf("%d", &t);
+    fastread(&t);
 	int a;
 	for (a = 0; a < t; a++) {
 		int n, m;
-		scanf("%d %d", &n, &m);
+        fastread(&n);
+        fastread(&m);
 	    int arr[m] ;
         int j;
         for (j = 0; j < m; j++){
-            scanf("%d", &arr[j]);
+            fastread(arr+j);
         }
         qsort(arr, m, 4, compare);
         int dup = m;
