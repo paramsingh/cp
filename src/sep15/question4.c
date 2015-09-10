a77271f9727de0d5a546b784dad4f406ac68faec
Param Singh <paramsingh258@gmail.com>
2015-09-11 01:56:56 +0530

    Rewrite entire code because logic was incorrect.

--- src/sep15/question4.c
+++ src/sep15/question4.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <limits.h>
 
-struct island {
-    int index;
-    int x;
-    int y;
-};
+typedef struct island {
+    int x, y, index;
+} island;
 
 int main(void) {
     int t;
@@ -15,158 +13,100 @@ int main(void) {
     for (a = 0; a < t; a++) {
         int num;
         scanf("%d", &num);
-        struct island maxx, maxy, minx, miny;
-        maxx.x = INT_MIN;
-        maxy.y = INT_MIN;
-        minx.x = INT_MAX;
-        miny.y = INT_MAX;
-        int i, x, y;
-        for (i = 1; i <= num; i++) {
+        int i;
+        island* arr  = (island *) calloc(num, 12);
+        island* maxx = (island *) calloc(num, 12);
+        island* minx = (island *) calloc(num, 12);
+        int xmax = INT_MIN, xmin = INT_MAX, ymax = INT_MIN, ymin = INT_MAX;
+        int cmaxx = 0, cmaxy = 0, cminx = 0, cminy = 0;
+        int x, y;
+        for (i = 0; i < num; i++) {
             scanf("%d %d", &x, &y);
-            if (maxx.x < x) {
-                maxx.x = x;
-                maxx.y = y;
-                maxx.index = i;
+            arr[i].x = x;
+            arr[i].y = y;
+            arr[i].index = i + 1;
+            if (xmax < x)
+                xmax = x;
+            if (xmin > x)
+                xmin = x;
+            if (ymax < y)
+                ymax = y;
+            if (ymin > y)
+                ymin = y;
+        }
+        int f = 0, index = -1, ans = 0;
+        for (i = 0; i < num; i++) {
+            x = arr[i].x;
+            y = arr[i].y;
+            if (x == xmax) {
+                if (y == ymax) {
+                    ans = 1;
+                    index = i+1;
+                    f = 1;
+                    break;
+                    // direction = SW
+                }
+                if (y == ymin) {
+                    ans = 1;
+                    index = i+1;
+                    f = 2;
+                    break;
+                    // direction = NW
+                }
+                maxx[cmaxx].x = x;
+                maxx[cmaxx].y = y;
+                maxx[cmaxx].index = i+1;
+                cmaxx++;
             }
-            if (minx.x > x) {
-                minx.x = x;
-                minx.y = y;
-                minx.index = i;
-            }
-            if (maxy.y < y) {
-                maxy.x = x;
-                maxy.y = y;
-                maxy.index = i;
-            }
-            if (miny.y > y) {
-                miny.x = x;
-                miny.y = y;
-                miny.index = i;
+            if (x == xmin) {
+                if (y == ymax) {
+                    ans = 1;
+                    index = i+1;
+                    f = 3;
+                    break;
+                    // direction = SE
+                }
+                if (y == ymin) {
+                    ans = 1;
+                    index = i+1;
+                    f = 4;
+                    break;
+                    // direction = NE
+                }
+                minx[cminx].x = x;
+                minx[cminx].y = y;
+                minx[cminx].index = i+1;
+                cminx++;
             }
         }
-        int ax = 0, ay = 0;
-        int fx = 0, fy = 0;
-        if (minx.y < maxx.y) {
-            if (minx.y == miny.y) {
-                // minx NE
-                fx = 1;
-                ax = 1;
-            }
-            else if (maxx.y == maxy.y) {
-                // maxx SW
-                fx = 2;
-                ax = 1;
-            }
-            else {
-                // minx NE
-                // maxx SW
-                fx = 3;
-                ax = 2;
-            }
-        }
-        else {
-            if (minx.y == maxy.y) {
-                // minx SE
-                fx = 4;
-                ax = 1;
-            }
-            else if (maxx.y == miny.y) {
-                // maxx NW
-                fx = 5;
-                ax = 1;
-            }
-            else {
-                // minx SE
-                // maxx NW
-                fx = 6;
-                ax = 2;
-            }
-        }
-        if (maxy.x < miny.x) {
-            if (maxy.x == minx.x) {
-                // maxy SE
-                fy = 1;
-                ay = 1;
-            }
-            else if (miny.x == maxx.x) {
-                // miny NW
-                fy = 2;
-                ay = 1;
-            }
-            else {
-                // maxy SE
-                // miny NW
-                fy = 3;
-                ay = 2;
-            }
-        }
-        else {
-            if (maxy.x == maxx.x) {
-                // maxy SW
-                fy = 4;
-                ay = 1;
-            }
-            else if (miny.x == minx.x) {
-                // miny NE
-                fy = 5;
-                ay = 1;
-            }
-            else {
-                // maxy SW
-                // miny NE
-                fy = 6;
-                ay = 2;
-            }
-        }
-        if (ax < ay) {
-            printf("%d\n", ax);
-            switch(fx) {
-                case 1:
-                    printf("%d NE\n", minx.index);
-                    break;
-                case 2:
-                    printf("%d SW\n", maxx.index);
-                    break;
-                case 3:
-                    printf("%d NE\n", minx.index);
-                    printf("%d SW\n", maxx.index);
-                    break;
-                case 4:
-                    printf("%d SE\n", minx.index);
-                    break;
-                case 5:
-                    printf("%d NW\n", maxx.index);
-                    break;
-                case 6:
-                    printf("%d SE\n", minx.index);
-                    printf("%d NW\n", maxx.index);
-                    break;
-            }
-        }
-        else {
-            printf("%d\n", ay);
-            switch(fy) {
-                case 1:
-                    printf("%d SE\n", maxy.index);
-                    break;
-                case 2:
-                    printf("%d NW\n", miny.index);
-                    break;
-                case 3:
-                    printf("%d SE\n", maxy.index);
-                    printf("%d NW\n", miny.index);
-                    break;
-                case 4:
-                    printf("%d SW\n", maxy.index);
-                    break;
-                case 5:
-                    printf("%d NE\n", miny.index);
-                    break;
-                case 6:
-                    printf("%d SW\n", maxy.index);
-                    printf("%d NE\n", miny.index);
-                    break;
-            }
+        if (f == 0)
+            ans = 2;
+        printf("%d\n", ans);
+        switch(f) {
+            case 1:
+                printf("%d SW\n", index);
+                break;
+            case 2:
+                printf("%d NW\n", index);
+                break;
+            case 3:
+                printf("%d SE\n", index);
+                break;
+            case 4:
+                printf("%d NE\n", index);
+                break;
+            case 0:
+                if (maxx[0].y > minx[0].y) {
+                    printf("%d SW\n", maxx[0].index);
+                    printf("%d NE\n", minx[0].index);
+                }
+                else {
+                    printf("%d NW\n", maxx[0].index);
+                    printf("%d SE\n", minx[0].index);
+                }
+                break;
         }
     }
 }
+
+
