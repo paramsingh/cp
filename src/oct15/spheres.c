981130d1c025e5052301953524c71d7e415156f9
Param Singh <paramsingh258@gmail.com>
2015-10-04 03:15:22 +0530

    Getting some wrong answers (15 points on Spheres)

---
+++ src/oct15/spheres.c
@@ -0,0 +1,44 @@
+// Author: Param Singh <paramsingh258@gmail.com>
+// Spheres: https://www.codechef.com/OCT15/problems/KSPHERES
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int n, m, c;
+    scanf("%d %d %d", &n, &m, &c);
+    long long int* spheres = (long long int *) calloc(c, 8);
+    long long int* dup = (long long int *) calloc(c, 8);
+    long long int *u = (long long int *) calloc(c, 8);
+    long long int *l = (long long int *) calloc(c, 8);
+    int i, j;
+    long long int x;
+    for (i = 0; i < n; i++) {
+        scanf("%lld", &x);
+        u[x-1]++;
+    }
+    for (i = 0; i < m; i++) {
+        scanf("%lld", &x);
+        l[x-1]++;
+        dup[x-1] = spheres[x-1] = l[x-1] * u[x-1];
+    }
+    long long int sum;
+    long long int aux;
+    for (j = 0; j < c; j++) {
+        sum = 0;
+        aux = 0;
+        for (i = 0; i < c; i++) {
+            // dup[i] contains number of j-spheres possible
+            // multiplying sum with dup[i] gives number of (j+1)-spheres
+            // possible with radius i+1;
+            long long int y = dup[i];
+            dup[i] = spheres[i] * sum;
+            aux += dup[i];
+            sum += y;
+        }
+        printf("%lld ", aux % 1000000007);
+    }
+    return 0;
+}
+
+
