44c997cf36e2614d8edc4f2099b505d30cca84cd
Param Singh <paramsingh258@gmail.com>
2015-10-03 19:29:12 +0530

    Equilateral Triangles (OCT15): 100 points

--- src/oct15/hypotenuse.c
+++ src/oct15/hypotenuse.c
@@ -4,39 +4,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void) {
-    int* seive = (int *) calloc(5000001, 4);
     int* arr   = (int *) calloc(5000001, 4);
     int i;
     int count = 0;
+    int flag;
     for (i = 1; i < 5000001; i++) {
         if (arr[i] == 0) {
-            if ((i+1) % 4 == 1)
-                seive[count++] = i+1;
+            if ((i+1) % 4 == 1) {
+                flag = 2;
+            }
+            else
+                flag = 1;
             int num = i+1;
             int x = 1;
             while (num < 5000001) {
-                arr[num-1] = 1;
-                num = num * (x++);
+                if (arr[num-1] != 2)
+                    arr[num-1] = flag;
+                num = num + i + 1;
             }
         }
     }
     int t;
     scanf("%d", &t);
     int a;
+    count = 0;
     for (a = 1; a <= t; a++) {
         int n;
         scanf("%d", &n);
-        int c = 0, flag = 0;
-        while (seive[c] <= n) {
-            if (seive[c] == 0)
-                break;
-            if (n % seive[c] == 0) {
-                flag = 1;
-                break;
-            }
-            c++;
-        }
-        if (flag)
+        if (arr[n-1] == 2)
             printf("YES\n");
         else
             printf("NO\n");
