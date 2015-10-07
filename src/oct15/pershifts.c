78fdc78ab46dd7f764c880f9bf425ff7c1e91c72
Param Singh <paramsingh258@gmail.com>
2015-10-07 12:24:36 +0530

    20 points for PERSHFTS.
    Now have to mess with modular arithmetic and then think about
    how to do it faster

--- src/oct15/pershifts.c
+++ src/oct15/pershifts.c
@@ -1,15 +1,46 @@
+// Author: Param Singh <paramsingh258@gmail.com>
+// Permutations: https://www.codechef.com/OCT15/problems/PERSHFTS
 #include <stdio.h>
 #include <stdlib.h>
 #define BIG 1000000007
 
-long long int msort(int* a, int lo, int hi);
-long long int merge(int* a, int lo, int hi, int mid);
+int k;
+int msort(int* a, int lo, int hi);
+int merge(int* a, int lo, int hi, int mid);
+void populate(int* count, int* a, int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        count[a[i]]++;
+    for (i = 1; i < n; i++)
+        count[i] += count[i-1];
+}
+
+void update(int* count, int a, int n) {
+    int i;
+    for (i = a; i < n; i++)
+        count[i]--;
+}
+
+int findrank(int* a, int n, int mul) {
+    int len = n;
+    int rank = 1, i;
+    int *count = (int *) calloc(n, 4);
+    populate(count, a, n);
+    for (i = 0; i < n; i++) {
+        mul /= len - i;
+        rank += count[a[i] - 1] * mul;
+        update(count, a[i], n);
+    }
+    return rank;
+}
+
 
 int main(void) {
-    int *fact = (int *) calloc(100001, 4);
     int a;
+    int *fact = (int *) calloc(100001, 4);
     fact[0] = 1;
-    for (a = 1; a < 100001; a++) fact[a] = (a * fact[a-1]) % BIG;
+    for (k = 1; k < 100001; k++)
+        fact[k] = (k * fact[k-1]) % BIG;
     int t;
     scanf("%d", &t);
     for (a = 0; a < t; a++) {
@@ -53,28 +84,38 @@ int main(void) {
                 printf("-1\n");
         }
         else if (k % 2 == 0) {
-            int ans = 0;
-            for (i = 0; i < n; i++) {
+            printf("%d\n", findrank(q, n, fact[n]));
+        }
+        else if (k % 2 != 0) {
+            int r = findrank(q, n, fact[n]);
+            int x = msort(p, 0, n-1);
+            int y = msort(q, 0, n-1);
+            if (x % 2 != y % 2)
+                printf("-1\n");
+            else {
+                if (r % 2 == 0)
+                    printf("%d\n", r/2);
+                else
+                    printf("%d\n", (r+1)/2);
             }
-            printf("%d\n", ans);
         }
     }
 }
 
 // function returns the number of inversions in between a[lo] to a[hi]
-long long int msort(int *a, int lo, int hi) {
+int msort(int *a, int lo, int hi) {
     int mid = lo + (hi - lo) / 2;
     // base case
     if (hi - lo == 0) return 0;
 
-    long long int left_inv = msort(a, lo, mid);
-    long long int right_inv = msort(a, mid+1, hi);
-    long long int split = merge(a, lo, hi, mid);
+    int left_inv = msort(a, lo, mid);
+    int right_inv = msort(a, mid+1, hi);
+    int split = merge(a, lo, hi, mid);
 
-    return (left_inv + right_inv + split);
+    return (left_inv + right_inv + split) % BIG;
 }
 
-long long int merge(int* a, int lo, int hi, int mid) {
+int merge(int* a, int lo, int hi, int mid) {
     // a[lo] to a[mid] has first sorted array
     // a[mid+1] to a[hi] has second sorted array
 
@@ -87,7 +128,7 @@ long long int merge(int* a, int lo, int hi, int mid) {
     // begin merge
     int* aux = (int *) calloc(size, sizeof(int));   // auxiliary array
     int left = lo, right = mid+1, left_overflow = 0, right_overflow = 0;
-    long long int count = 0, rem_left = mid - lo + 1;
+    int count = 0, rem_left = mid - lo + 1;
     for (i = 0; i < size; i++) {
         if (right_overflow || (a[left] <= a[right] && !left_overflow)) {
             aux[i] = a[left++];
@@ -101,7 +142,7 @@ long long int merge(int* a, int lo, int hi, int mid) {
                 right_overflow = 1;
 
             if (!left_overflow)
-                count += rem_left;
+                count = (count + rem_left);
         }
     }
 
@@ -112,3 +153,4 @@ long long int merge(int* a, int lo, int hi, int mid) {
     free(aux);
     return count;
 }
+
