063fb8c0005886cea6180cdd9ac637d72838db46
Param Singh <paramsingh258@gmail.com>
2015-11-05 18:11:44 +0530

    Martian Mining on SPOJ done.

--- src/practice/martian.c
+++ src/practice/martian.c
@@ -12,32 +12,26 @@ int max(int a, int b) {
 
 int main(void) {
     int n, m;
-    int i, j;
+    int i, j, sum;
     scanf("%d %d", &n, &m);
     while (n != 0 && m != 0) {
         struct cell a[n][m];
         int top[n][m];
         int left[n][m];
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) {
-                scanf("%d", &(a[i][j].up));
-                printf("%d\t", a[i][j].up);
-            }
-            printf("\n");
-        }
-        printf("\n");
-        int sum;
+        int dp[n][m];
         for (i = 0; i < n; i++) {
             sum = 0;
             for (j = 0; j < m; j++) {
                 scanf("%d", &(a[i][j].left));
                 sum += a[i][j].left;
                 left[i][j] = sum;
-                printf("%d\t", a[i][j].left);
             }
-            printf("\n");
         }
-        printf("\n");
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                scanf("%d", &(a[i][j].up));
+            }
+        }
         for (j = 0; j < m; j++) {
             sum = 0;
             for (i = 0; i < n; i++) {
@@ -45,20 +39,6 @@ int main(void) {
                 top[i][j] = sum;
             }
         }
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) {
-                printf("%d\t", top[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) {
-                printf("%d\t", left[i][j]);
-            }
-            printf("\n");
-        }
-        int dp[n][m];
         for (i = 0; i < n; i++) {
             for (j = 0; j < m; j++) {
                 if (i == 0 && j == 0)
@@ -71,16 +51,8 @@ int main(void) {
                     dp[i][j] = max(left[i][j] + dp[i-1][j], top[i][j] + dp[i][j-1]);
             }
         }
-        printf("\n");
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) {
-                printf("%d\t", dp[i][j]);
-            }
-            printf("\n");
-        }
         printf("%d\n", dp[n-1][m-1]);
         scanf("%d %d", &n, &m);
     }
 }
-
 
