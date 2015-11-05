390063a13d727d88912cd9edce63c8a70c96403b
Param Singh <paramsingh258@gmail.com>
2015-11-05 16:07:01 +0530

    More work on Martian, better recurrence

--- src/practice/martian.c
+++ src/practice/martian.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dpcell {
+struct cell {
     int up;
     int left;
-};
-
-struct cell {
-    int y;
-    int b;
 };
 
 int max(int a, int b) {
@@ -20,54 +15,72 @@ int main(void) {
     int i, j;
     scanf("%d %d", &n, &m);
     while (n != 0 && m != 0) {
-        struct cell arr[n][m];
-        struct dpcell dp[n][m];
-        struct dpcell sums[n][m];
-        int sum = 0;
+        struct cell a[n][m];
+        int top[n][m];
+        int left[n][m];
         for (i = 0; i < n; i++) {
             for (j = 0; j < m; j++) {
-                scanf("%d", &(arr[i][j].y));
-                sum += arr[i][j].y;
+                scanf("%d", &(a[i][j].up));
+                printf("%d\t", a[i][j].up);
             }
+            printf("\n");
         }
-        for (i = 0; i < n; i++)
-            for (j = 0; j < m; j++)
-                scanf("%d", &(arr[i][j].b));
-        for (j = 0; j < m; j++) {
-            for (i = n-1; i >= 0; i--) {
-                if (i == n-1)
-                    sums[i][j].left = arr[i][j].b;
-                else
-                    sums[i][j].left = arr[i][j].b + sums[i+1][j].left;
-            }
-        }
-
+        printf("\n");
+        int sum;
         for (i = 0; i < n; i++) {
-            for (j = m-1; j >= 0; j--) {
-                if (j == m-1)
-                    sums[i][j].up = arr[i][j].y;
-                else
-                    sums[i][j].up = arr[i][j].y + sums[i][j+1].up;
+            sum = 0;
+            for (j = 0; j < m; j++) {
+                scanf("%d", &(a[i][j].left));
+                sum += a[i][j].left;
+                left[i][j] = sum;
+                printf("%d\t", a[i][j].left);
             }
+            printf("\n");
         }
-
+        printf("\n");
         for (j = 0; j < m; j++) {
+            sum = 0;
             for (i = 0; i < n; i++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j].up = sum;
-                    dp[i][j].left = sum - sums[i][j].up + sums[i][j].left;
-                }
-                else if (i == 0) {
-                    dp[i][j].left = dp[i][j-1].left - sums[;
-                else {
-                    dp[i][j].up = dp[i-1][j].up;
-                    dp[i][j].left = max(dp[i-1][j].up, dp[i-1][j].left);
-                }
+                sum += a[i][j].up;
+                top[i][j] = sum;
             }
         }
-
-        printf("%d\n", max(dp[n-1][m-1].left, dp[n-1][m-1].up));
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                printf("%d\t", top[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                printf("%d\t", left[i][j]);
+            }
+            printf("\n");
+        }
+        int dp[n][m];
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                if (i == 0 && j == 0)
+                    dp[i][j] = max(top[i][j], left[i][j]);
+                else if (i == 0)
+                    dp[i][j] = max(top[i][j] + dp[i][j-1], left[i][j]);
+                else if (j == 0)
+                    dp[i][j] = max(left[i][j] + dp[i-1][j], top[i][j]);
+                else
+                    dp[i][j] = max(left[i][j] + dp[i-1][j], top[i][j] + dp[i][j-1]);
+            }
+        }
+        printf("\n");
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                printf("%d\t", dp[i][j]);
+            }
+            printf("\n");
+        }
+        printf("%d\n", dp[n-1][m-1]);
         scanf("%d %d", &n, &m);
     }
 }
+
 
