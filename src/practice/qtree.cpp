8247a3b1138475c63c6ed052bafe06dbe3b54737
Param Singh <paramsingh258@gmail.com>
2015-12-23 23:26:56 +0530

    QTREE on SPOJ: http://www.spoj.com/problems/QTREE
    Heavy light decomposition

--- src/practice/qtree.cpp
+++ src/practice/qtree.cpp
@@ -5,7 +5,7 @@
 #include <cstdlib>
 #include <vector>
 #include <climits>
-#define DEPTH 15
+#define DEPTH 14
 #define SIZE 10010
 #define max(a, b) a>b?a:b
 using namespace std;
@@ -21,33 +21,25 @@ struct node {
     int pos;         // position of node in the base array
     int subsize;     // size of subtree under the current node
     int depth;       // depth of the node in the tree
-};
+} tree[SIZE];
 
-struct node* tree;   // array of nodes for the tree
-int* base;           // base array used for construction of segment tree
+int base[SIZE];           // base array used for construction of segment tree
+int end[SIZE];            // array to store the end of edge used to represent
+                     // the edge in the base array
 
-// struct for a chain in the tree
-struct chain {
-    int head;        // the node which is the head of the chain
-    int size;        // size of the chain
-};
+int head[SIZE];      // used to store the head of each chain
 
-struct chain* chains; // array of chains
 int pa[DEPTH][SIZE];  // pa[i][j] = (2^i)th ancestor of j
 
 // dfs sets up the subsize, depth and base cases (1 height) of the pa array
 void dfs(int cur, int prev, int depth) {
-    // chain properties will be initialized later in the hld function
-    tree[cur].cid = -1;
-    tree[cur].pos = -1;
     tree[cur].subsize = 1;   // include the current node in the subsize
     tree[cur].depth = depth; // depth of node is the current depth
     pa[0][cur] = prev;       // ancestor at height 0 is the node with which we came here
-    printf("pa[0][%d] = %d\n", cur, pa[0][cur]);
     int i;
     for (i = 0; i < tree[cur].adj.size(); i++) {
-        printf("%d -> %d\n", cur, tree[cur].adj[i]);
         if (tree[cur].adj[i] != prev) {
+            end[tree[cur].id[i]] = tree[cur].adj[i];
             dfs(tree[cur].adj[i], cur, depth+1); // dfs
             tree[cur].subsize += tree[tree[cur].adj[i]].subsize; // add subsize of child
         }
@@ -61,8 +53,8 @@ int bs  = 0;          // variable to keep track of positions in base array
 // sets up cid and pos for each node and sets up the base array
 void hld(int cur, int prev, int wt) {
     // if the head of the current chain is not set, set it to current node
-    if (chains[cno].head == 0)
-        chains[cno].head = cur;
+    if (head[cno] == -1)
+        head[cno] = cur;
     tree[cur].cid = cno; // set the chain id of current node
     tree[cur].pos = bs;  // set the position of current node in base array
     base[bs++] = wt;     // add value to base array
@@ -93,7 +85,7 @@ void hld(int cur, int prev, int wt) {
     }
 }
 
-int segtree[4*SIZE]; // segment tree on base array for maximum in a range
+int segtree[6*SIZE]; // segment tree on base array for maximum in a range
 
 // segment tree operations
 void build(int i, int l, int r) {
@@ -136,7 +128,7 @@ int query(int l, int r, int i, int cl, int cr) {
 }
 
 // function to setup parent array
-void setup_parents() {
+inline void setup_parents() {
     int i, j;
     // row zero has been set by dfs
     // therefore we know the immediate parents
@@ -156,36 +148,76 @@ int lca(int u, int v) {
         u = v;
         v = temp;
     }
-    int i;
-    for (i = DEPTH; i >= 0; i--) {
+    int i, log = 0;
+    for (i = 13; i >= 0; i--) {
         if (tree[u].depth - (1 << i) >= tree[v].depth) {
             u = pa[i][u];
         }
     }
-
     if (u == v)
         return v;
-
-    for (i = DEPTH; i >= 0; i--) {
+    for (i = 13; i >= 0; i--) {
         if (pa[i][u] != -1 && pa[i][u] != pa[i][v]) {
             u = pa[i][u];
             v = pa[i][v];
         }
     }
-
     return pa[0][u];
 }
 
+int query_up(int u, int v) {
+    // guaranteed that v is an ancestor of u
+    if (u == v)
+        return 0;
+    int uchain = tree[u].cid, vchain = tree[v].cid;
+    int ans = -1;
+
+    while (true) {
+        uchain = tree[u].cid;
+        if (uchain == vchain) {
+            int x = tree[u].pos;
+            int y = tree[v].pos;
+            int a = query(y+1, x, 1, 0, bs-1);
+            if (ans < a)
+                ans = a;
+            break;
+        }
+        else {
+            int h = head[uchain];
+            int a = query(tree[h].pos, tree[u].pos, 1, 0, bs-1);
+            if (ans < a)
+                ans = a;
+            u = pa[0][h];
+        }
+    }
+    return ans;
+}
+
+int query1(int u, int v) {
+    int ancestor = lca(u, v);
+    return max(query_up(u, ancestor), query_up(v, ancestor));
+}
+
+void update1(int i, int val) {
+    int x = end[i];
+    update(1, 0, bs-1, tree[x].pos, val);
+}
+
 int main(void) {
-    int t;
+    int t, i, j;
     scanf("%d", &t);
     while (t--) {
         scanf("%d", &n);
-        tree = (struct node*) calloc(n+10, sizeof(struct node));
-        chains = (struct chain*) calloc(n+10, sizeof(struct chain));
-        base = (int *) calloc(n+10, sizeof(int));
-        int i, a, b, c, j;
+        for (i = 0; i <= n; i++) {
+            tree[i].adj.clear();
+            tree[i].wt.clear();
+            tree[i].id.clear();
+            head[i] = -1;
+            for (j = 0; j < DEPTH; j++)
+                pa[j][i] = -1;
+        }
         for (i = 1; i < n; i++) {
+            int a, b, c;
             scanf("%d %d %d", &a, &b, &c);
             tree[a].adj.push_back(b);
             tree[a].wt.push_back(c);
@@ -194,14 +226,28 @@ int main(void) {
             tree[b].wt.push_back(c);
             tree[b].id.push_back(i);
         }
-        for (i = 0; i < DEPTH; i++)
-            for (j = 0; j < n; j++)
-                pa[i][j] = -1;
+        bs = 0;
+        cno = 0;
         dfs(1, -1, 0);
-        hld(1, -1, -1);
-        build(1, 0, bs);
+        hld(1, -1, 0);
+        build(1, 0, bs-1);
         setup_parents();
-        printf("%d\n", query1(2, 3));
+        char choice[100];
+        int u, v;
+        while (true) {
+            scanf("%s", choice);
+            if (choice[0] == 'D')
+                break;
+            else if (choice[0] == 'Q') {
+                scanf("%d %d", &u, &v);
+                int ans = query1(u, v);
+                printf("%d\n", ans);
+            }
+            else {
+                scanf("%d %d", &u, &v);
+                update1(u, v);
+            }
+        }
     }
     return 0;
 }
