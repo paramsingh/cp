0b1de9fcfb3e152dd9676cb596098af2f335f224
Param Singh <paramsingh258@gmail.com>
2016-07-22 04:07:35 +0530

    Remove never used folders

--- utilities/pattern_generator_frmq.py
+++
@@ -1,5 +0,0 @@
-n, x, l, r = map(int, raw_input().split())
-for i in range(x):
-    print (l, r), "\t", (min(l,r), max(l,r))
-    l = (l+7) % (n-1)
-    r = (r+11) % n
--- workspace/digvijay.py
+++
@@ -1,9 +0,0 @@
-terms = int(raw_input())
-for q in range(terms):
-    base, mod = map(int, raw_input().split())
-    string = raw_input()
-    num = 0
-    for ch in string:
-        num = num * base + int(ch)
-        print (num % mod),
-    print ""
